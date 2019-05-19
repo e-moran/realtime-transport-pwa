c6d6f05ed2aa3d844e1c0e30eb9779f9f172acaf
Eoin Moran <eoin.t.moran00@gmail.com>
2019-05-19 19:49:33 +0100

    Added Sidebar and Navbar

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent {
+
+  @Input()
+  isHandset$: Observable<boolean>;
+
+  constructor() {}
+
+}
